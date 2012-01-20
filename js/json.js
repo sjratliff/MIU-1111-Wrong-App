3acc5685e448039fc7f3cb40b3c74d7bb449de36
Stephen Ratliff <sjratliff@me.com>
2012-01-19 18:53:22 -0700

    Bad App Done Week 3

---
+++ js/json.js
@@ -0,0 +1,326 @@
+
+//JSON 
+
+
+//JSON Object Which will auto populate local storage.
+	function autoFillData(){
+		var json = {
+			"contact1":	{
+				"group": 	["Day:", "Friday"],
+				"tname": 	["Name:","Sean Combs"],
+				"sex":   	["Sex:","Male"],
+				"startdate":["Birthday:","1969-11-4"],
+				"pword":	["People In Party:","40"],
+				"cpword":	["Phone Number:","212-555-1212"],
+				"email":	["Email:","pdiddy@badboyent.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Record Release Party"]
+	
+			},
+			"contact2":	{
+				"group": 	["Day:", "Saturday"],
+				"tname": 	["Name:","Tyrese Gibson"],
+				"sex":   	["Sex:","Male"],
+				"startdate":["Birthday:","1978-12-30"],
+				"pword":	["People In Party:","15"],
+				"cpword":	["Phone Number:","212-555-2345"],
+				"email":	["Email:","blacktye@hasbro.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","30th Birthday"]
+			},
+			"contact3":{
+				"group": 	["Day:", "Thursday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact4":{
+				"group": 	["Day:", "Thursday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact5":{
+				"group": 	["Day:", "Friday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact6":{
+				"group": 	["Day:", "Thursday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact7":{
+				"group": 	["Day:", "Saturday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact8":{
+				"group": 	["Day:", "Thursday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact9":{
+				"group": 	["Day:", "Saturday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact10":{
+				"group": 	["Day:", "Thursday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact11":	{
+				"group": 	["Day:", "Saturday"],
+				"tname": 	["Name:","Tyrese Gibson"],
+				"sex":   	["Sex:","Male"],
+				"startdate":["Birthday:","1978-12-30"],
+				"pword":	["People In Party:","15"],
+				"cpword":	["Phone Number:","212-555-2345"],
+				"email":	["Email:","blacktye@hasbro.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","30th Birthday"]
+			},
+			"contact12":{
+				"group": 	["Day:", "Thursday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact13":{
+				"group": 	["Day:", "Friday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact14":{
+				"group": 	["Day:", "Friday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact15":{
+				"group": 	["Day:", "Saturday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact16":{
+				"group": 	["Day:", "Thursday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact17":{
+				"group": 	["Day:", "Thursday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact18":{
+				"group": 	["Day:", "Friday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact19":{
+				"group": 	["Day:", "Thursday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+			},
+			"contact20":{
+				"group": 	["Day:", "Friday"],
+				"tname": 	["Name:","Elizabeth Shue"],
+				"sex":   	["Sex:","Female"],
+				"startdate":["Birthday:","1963-10-6"],
+				"pword":	["People In Party:","12"],
+				"cpword":	["Phone Number:","312-555-5423"],
+				"email":	["Email:","jordanMooney@cocktailsanddreams.com"],
+				"rating":	["Rating:","8"],
+				"comments":	["Comments:","Divorce Party"]
+
+			}
+				
+		};
+		//store the JSON OBJECT into local storage
+		for(var n in json){
+			var id              = Math.floor(Math.random()*100000001);
+			localStorage.setItem(id, JSON.stringify(json[n]));
+		}
+	}
+    //Make Item Links
+    //Create the edit and delete links for each stored item when displayed
+    function makeItemLinks(key, linksLi){
+    //add edit single item link
+    var editLink = document.createElement('a');
+    editLink.href = '#';
+    editLink.key = key;
+    var editText = "Edit Info";
+    editLink.addEventListener("click", editItem);
+    editLink.innerHTML = editText;
+    linksLi.appendChild(editLink);
+    
+    //add line break
+    var breakTag = document.createElement('br');
+    linksLi.appendChild(breakTag);
+    
+    
+    
+    //add delete single item link
+    var deleteLink = document.createElement('a');
+    deleteLink.href = "#";
+    deleteLink.key = key;
+    var deleteText = "Delete Info";
+    deleteLink.addEventListener("click", deleteItem);
+    deleteLink.innerHTML = deleteText;
+    linksLi.appendChild(deleteLink);
+    
+    }
+    
+    function editItem(){
+    //Grab the data from our item from local Storage
+        var value = localStorage.getItem(this.key);
+        var item = JSON.parse(value);
+        
+        //Show the form
+        toggleControls("off");
+        
+        //populate the form fields with the current localStorage values
+        $('groups')   .value = item.group[1];
+        $('tname')    .value = item.tname[1];
+        var radios = document.forms[0].sex;
+        for(var i=0; i<radios.length;i++){
+            if(radios[i].value == "male" && item.sex[1] == "male"){
+                radios[i].setAttribute("checked", "checked");
+           }else if (radios[i].value == "female" && item.sex[1] == "female"){
+               radios[i].setAttribute("checked" , "checked");
+               }
+           }
+        $('startdate').value = item.date[1];
+        $('pword')    .value = item.pword[1];
+        $('cpword')   .value = item.cpword[1];
+        $('email')    .value = item.email[1];
+        $('rating')   .value = item.rating[1];
+        $('comments') .value = item.comments[1];
+        
+        
+        //Remove the initial listener from the  input "save contact" button
+        save.removeEventListener("click", storeData);
+        //Change Submit Button Value to Edit Button
+        $('submit').value = "Edit Contact";
+        var editSubmit = $('submit');
+        //Save the key value established in this function as a property of the editSubmit event
+        //soo we can use that value when we save the data we edited.
+        editSubmit.addEventListener("click" , validate);
+        editSubmit.key = this.key;
+        
+        
+    }
+    
+    function deleteItem(){
+    	var ask = confirm("Are you Sure you Want To delete?");
+    		if(ask){
+    		 localStorage.removeItem(this.key);
+    		 window.location.reload();
+    		}else{
+    			alert("Information Was NOT deleted.")
+    		}
+    }
+    
+    function clearLocal(){
+        if(localStorage.length === 0 ){
+            alert("There is no data to clear.");
+        }else{
+            localStorage.clear();
+            alert("All Info Has Been Cleared");
+            window.location.reload();
+            return false;
+        }
+    }
